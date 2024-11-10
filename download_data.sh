f6d8779f4d041fd79b56193ae93bda038688c21e
root <ragy202@gmail.com>
2024-11-10 18:32:43 +0000

    update download scripts

--- download_data.sh
+++ download_data.sh
@@ -1,3 +1,8 @@
 mkdir -p datasets
 wget https://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/complete/uniprot_sprot.fasta.gz  -O datasets/uniprot_sprot.fasta.gz  
 gunzip datasets/uniprot_sprot.fasta.gz 
+
+wget https://raw.githubusercontent.com/charlesxu90/helm-gpt/refs/heads/main/data/prior/monomer_library.csv -O datasets/monomer_library.csv
+
+wget https://raw.githubusercontent.com/charlesxu90/helm-gpt/refs/heads/main/data/prior/chembl32/biotherapeutics_dict_prot_flt.csv -O datasets/biotherapeutics_dict_prot_flt.csv 
+
